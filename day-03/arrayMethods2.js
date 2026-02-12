96b6e23468d7dbb8b70d6616b5bfff4df166c8e9
muhammadsaad <muhammadsaaad64@gmail.com>
2026-02-12 13:09:00 +0500

    day-03 learning array mehtods without mutating orignal

---
+++ day-03/arrayMethods2.js
@@ -0,0 +1,6 @@
+let orders = [
+  { id: 1, userId: 101, items: [{name:"Laptop", price:1000, quantity:1}], status: "pending", total:1000 },
+  { id: 2, userId: 102, items: [{name:"Shirt", price:50, quantity:2}], status: "pending", total:100 },
+  { id: 3, userId: 101, items: [{name:"Phone", price:500, quantity:1}], status: "shipped", total:500 },
+  { id: 4, userId: 103, items: [{name:"Shoes", price:120, quantity:1}], status: "pending", total:120 },
+];
